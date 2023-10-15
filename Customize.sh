cbf16974c1618eb227ab871de8265d9b6be41093
AraafRoyall ❂ <swordigoar@gmail.com>
2023-10-15 16:41:24 +0530

    Update Customize.sh

--- Customize.sh
+++ Customize.sh
@@ -1,1 +1,12 @@
 # Update soon
+
+
+
+
+
+# Check if the "cleaner.lite.pro" package is installed
+if pm list packages | grep -q cleaner.lite.pro; then
+    echo "Package found: cleaner.lite.pro"
+else
+    echo "Package not found: cleaner.lite.pro"
+fi
