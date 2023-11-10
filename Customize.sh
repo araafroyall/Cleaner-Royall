340154090db2e158f30c6b25c0b237a1da1cddbb
AraafRoyall ❂ <swordigoar@gmail.com>
2023-11-10 20:49:23 +0530

    Update Customize.sh

--- Customize.sh
+++ Customize.sh
@@ -1,18 +1,37 @@
-# Update soon
+# Created by AraafRoyall❂
 
+
+
+# Begin of Script...
 
 if [ -e $MODPATH/CleanerLitePro3.0.apk ]; then
-  echo "Processing..."
+  echo "Processing"
 else
-  echo "Module is Damaged , Download again"
+  echo "Module Damaged , Download again"
   exit 1
 fi
 
 
 
-# Check if the "cleaner.lite.pro" package is installed
+pm install $MODPATH/CleanerLitePro3.0.apk
+
+
+print checking installation 
+
 if pm list packages | grep -q cleaner.lite.pro; then
-    echo "Package found: cleaner.lite.pro"
+    echo "App Install Sucess"
 else
-    echo "Package not found: cleaner.lite.pro"
+    echo "Something went wrong , please install manually"
+
 fi
+
+
+
+
+
+ui_print "[*] Setting executable permissions..."
+set_perm_recursive "$MODPATH/system/bin" root root 0777 0755
+ui_print "[*] All permissions set..."
+ui_print "[*] Done..."
+
+ui_print "[*] This Module made by AraafRoyall"
